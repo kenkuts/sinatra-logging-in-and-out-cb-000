fea8818e8ed615e16f6be6c20f12aeb2ed960656
kenkuts <kennethchoi19@gmail.com>
2018-12-20 23:36:53 -0500

    Completed Authentication

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,11 @@
 class Helpers
+
+  def self.current_user(session)
+    return User.find(session[:user_id])
+  end
+
+  def self.is_logged_in?(session)
+    return true if current_user(session) != nil
+  end
+
 end
